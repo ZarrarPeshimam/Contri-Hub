4e1cdc8a3d8942a329e5adbb6c475bebae34ec2a
Zarrar <zarrarpeshimam@gmail.com>
2026-06-21 19:14:18 +0530

    Updated DB connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,13 @@
 import mongoose from "mongoose";
 
-export default function connectDB() {
-  mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log("Mongo connected"))
-    .catch(err => console.error(err));
+export default async function connectDB() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      dbName: "contri-hub",
+    });
+
+    console.log("Mongo connected");
+  } catch (err) {
+    console.error(err);
+  }
 }
